900583744e4353e8e3929875508d19a2b1f7c78e
Erdvik <magnuserdvik@hotmail.com>
2021-03-26 11:22:21 +0100

    scaffolding av sider er ferdig

---
+++ Pages/Bookings/Create.cshtml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using EgdeBookingSystemV2.Data;
+using EgdeBookingSystemV2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EgdeBookingSystemV2.Pages.Bookings
+{
+    public class CreateModel : PageModel
+    {
+        private readonly EgdeBookingSystemV2.Data.EgdeBookingSystemConnection _context;
+
+        public CreateModel(EgdeBookingSystemV2.Data.EgdeBookingSystemConnection context)
+        {
+            _context = context;
+        }
+
+        public Equipment Equipment { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Equipment = await _context.Equipments
+                    .FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Equipment == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        [BindProperty]
+        public Booking Booking { get; set; }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Bookings.Add(Booking);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ Pages/UserView/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using EgdeBookingSystemV2.Data;
+using EgdeBookingSystemV2.Models;
+
+namespace EgdeBookingSystemV2.Pages.UserView
+{
+    public class IndexModel : PageModel
+    {
+        private readonly EgdeBookingSystemV2.Data.EgdeBookingSystemConnection _context;
+
+        public IndexModel(EgdeBookingSystemV2.Data.EgdeBookingSystemConnection context)
+        {
+            _context = context;
+        }
+
+        public IList<Equipment> Equipment { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Equipment = await _context.Equipments
+                .Include(e => e.Category)
+                .Include(e => e.Location).ToListAsync();
+        }
+    }
+}
