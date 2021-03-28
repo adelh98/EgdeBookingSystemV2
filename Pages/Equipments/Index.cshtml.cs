7c0b85b490d186321dc52b0d30271aac8eebcad3
Erdvik <magnuserdvik@hotmail.com>
2021-03-28 14:59:27 +0200

    lagt til noe filtrering

--- Pages/Equipments/Index.cshtml.cs
+++ Pages/Equipments/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using EgdeBookingSystemV2.Data;
 using EgdeBookingSystemV2.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace EgdeBookingSystemV2.Pages.Equipments
 {
@@ -19,10 +20,26 @@ namespace EgdeBookingSystemV2.Pages.Equipments
             _context = context;
         }
 
-        public IList<Equipment> Equipment { get; set; }
+
         public IList<Equipment> EquipmentSearch { get; set; }
+        public IList<Equipment> Equipment { get; set; }
+        public SelectList Categories { get; set; }
+        public SelectList Locations { get; set; }
+        public SelectList Status { get; set; }
+
         [BindProperty(SupportsGet = true)]
         public string SearchString { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string CategoryFilter { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string LocationFilter { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string BookedFilter { get; set; } = null;
+
+
 
         public async Task OnGetAsync()
         {
@@ -30,18 +47,40 @@ namespace EgdeBookingSystemV2.Pages.Equipments
                 .Include(e => e.Category)
                 .Include(e => e.Location).ToListAsync();
 
+            IQueryable<string> categoryQuery = from c in _context.Categories
+                                               orderby c.Name
+                                               select c.Name;
+            Categories = new SelectList(await categoryQuery.ToListAsync());
+
+            IQueryable<string> locationQuery = from l in _context.Locations
+                                               orderby l.Name
+                                               select l.Name;
+            Locations = new SelectList(await locationQuery.ToListAsync());
+
+            string[] Statuses = { "ledig", "opptatt" };
+            Status = new SelectList(Statuses);
+
+
             var equipments = from n in _context.Equipments
-                            select n;
+                             select n;
             if (!string.IsNullOrEmpty(SearchString))
             {
                 equipments = equipments.Where(s => s.Name.Contains(SearchString));
             }
+            if (!string.IsNullOrEmpty(CategoryFilter))
+            {
+                equipments = equipments.Where(s => s.Category.Name.Contains(CategoryFilter));
+            }
+            if (!string.IsNullOrEmpty(LocationFilter))
+            {
+                equipments = equipments.Where(s => s.Location.Name.Contains(LocationFilter));
+            }
 
             EquipmentSearch = await equipments.ToListAsync();
         }
+
     }
 }
-
 
 
 
