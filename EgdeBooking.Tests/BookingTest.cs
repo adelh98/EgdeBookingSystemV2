ef7d3123855fe5a77274587e475e63803cd93ed1
Erik <erikdn10@uia.no>
2021-04-30 17:11:29 +0200

    Har lagt til unit-testene som forsvant ved tidligere revert

---
+++ EgdeBooking.Tests/BookingTest.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using EgdeBookingSystemV2.Data;
+using EgdeBookingSystemV2.Models;
+using EgdeBookingSystemV2.Pages.Bookings;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure.Internal;
+using Moq;
+using Xunit;
+
+namespace EgdeBooking.Tests
+{
+    public class BookingsTest
+    {
+
+        [Fact]
+        public async Task GetDetailsModel_OnNullInput_ExpectNotFound()
+        {
+            var mockContext = new Mock<EgdeBookingSystemConnection>();
+            var detailsModel = new DetailsModel(mockContext.Object);
+            var page = await detailsModel.OnGetAsync(null);
+            page.Should().BeAssignableTo<NotFoundResult>();
+        }
+
+        [Fact]
+        public async Task GetDetailsModel_ReturnsOtherId_ExpectNotFound()
+        {
+            var options = new DbContextOptionsBuilder<EgdeBookingSystemConnection>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            await using (var context = new EgdeBookingSystemConnection(options))
+            {
+                await context.Bookings.AddAsync(new Booking
+                {
+                    ID = 2,
+                    UserEmail = "test@test.no",
+                    Equipment = new Equipment
+                    {
+                        ID = 2
+                    }
+                });
+                await context.SaveChangesAsync();
+            }
+
+            // Use a clean instance of the context to run the test
+            await using (var context = new EgdeBookingSystemConnection(options))
+            {
+
+                var detailsModel = new DetailsModel(context);
+                var page = await detailsModel.OnGetAsync(3);
+                page.Should().BeAssignableTo<NotFoundResult>();
+            }
+        }
+
+        [Fact]
+        public async Task GetDetailsModel_ReturnsModel_ExpectResult()
+        {
+            var options = new DbContextOptionsBuilder<EgdeBookingSystemConnection>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            await using (var context = new EgdeBookingSystemConnection(options))
+            {
+                await context.Bookings.AddAsync(new Booking
+                {
+                    ID = 2,
+                    UserEmail = "test@test.no",
+                    Equipment = new Equipment
+                    {
+                        ID = 2
+                    }
+                });
+                await context.SaveChangesAsync();
+            }
+
+            // Use a clean instance of the context to run the test
+            await using (var context = new EgdeBookingSystemConnection(options))
+            {
+
+                var detailsModel = new DetailsModel(context);
+                var page = await detailsModel.OnGetAsync(2);
+                page.Should().BeAssignableTo<PageResult>();
+            }
+        }
+    }
+}
